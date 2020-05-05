5fb88072eadd986936d9aa77391f14320eccb4b3
Rafael Borges <rafaelfborges@gmail.com>
2020-05-05 01:56:23 -0300

    Module 03: Utility - Attribute class calculator

---
+++ java-8/src/main/java/com/challenge/annotation/Somar.java
@@ -0,0 +1,4 @@
+package com.challenge.annotation;
+
+public @interface Somar {
+}
---
+++ java-8/src/main/java/com/challenge/desafio/CalculadorDeClasses.java
@@ -0,0 +1,24 @@
+package com.challenge.desafio;
+
+import com.challenge.interfaces.Calculavel;
+
+import java.math.BigDecimal;
+
+public class CalculadorDeClasses implements Calculavel {
+
+
+    @Override
+    public BigDecimal somar() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal subtrair() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal totalizar() {
+        return null;
+    }
+}
