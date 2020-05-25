9ca78037bff8f5a17cf61326fb3a49d404d01197
Rafael Borges <rafaelfborges@gmail.com>
2020-05-25 04:43:04 -0300

    Module 05 exercices finalize

--- java-15/src/main/java/br/com/codenation/service/OrderServiceImpl.java
+++ java-15/src/main/java/br/com/codenation/service/OrderServiceImpl.java
@@ -2,7 +2,9 @@ package br.com.codenation.service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import br.com.codenation.model.OrderItem;
 import br.com.codenation.model.Product;
@@ -11,14 +13,22 @@ import br.com.codenation.repository.ProductRepositoryImpl;
 
 public class OrderServiceImpl implements OrderService {
 
-	private ProductRepository productRepository = new ProductRepositoryImpl();
+	private final ProductRepository productRepository = new ProductRepositoryImpl();
 
 	/**
 	 * Calculate the sum of all OrderItems
 	 */
 	@Override
 	public Double calculateOrderValue(List<OrderItem> items) {
-		return null;
+		return items.stream()
+				.mapToDouble(orderItem -> {
+					Product product = productRepository.findById(orderItem.getProductId()).orElse(null);
+					if(product != null)
+						return product.getIsSale()
+								? 0.8 * (orderItem.getQuantity() * product.getValue())
+								: orderItem.getQuantity() * product.getValue();
+					return 0.0;
+				}).sum();
 	}
 
 	/**
@@ -26,7 +36,10 @@ public class OrderServiceImpl implements OrderService {
 	 */
 	@Override
 	public Set<Product> findProductsById(List<Long> ids) {
-		return null;
+		return ids.stream()
+				.filter(id -> productRepository.findById(id).isPresent())
+				.map(id -> productRepository.findById(id).orElse(null))
+				.collect(Collectors.toSet());
 	}
 
 	/**
@@ -34,7 +47,7 @@ public class OrderServiceImpl implements OrderService {
 	 */
 	@Override
 	public Double calculateMultipleOrders(List<List<OrderItem>> orders) {
-		return null;
+		return orders.stream().mapToDouble(this::calculateOrderValue).sum();
 	}
 
 	/**
@@ -42,7 +55,10 @@ public class OrderServiceImpl implements OrderService {
 	 */
 	@Override
 	public Map<Boolean, List<Product>> groupProductsBySale(List<Long> productIds) {
-		return null;
+
+		return productIds.stream()
+				.map(id -> productRepository.findById(id).orElse(null))
+				.collect(Collectors.groupingBy(Product::getIsSale));
 	}
 
 }
